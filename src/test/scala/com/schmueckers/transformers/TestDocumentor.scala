716edcbffef5cd1aa3f10a5cc4a278f67df89261
js <jschmuec@gmail.com>
2017-05-10 06:09:59 +0200

    got simple documentation going

--- src/test/scala/com/schmueckers/transformers/TestDocumentor.scala
+++ src/test/scala/com/schmueckers/transformers/TestDocumentor.scala
@@ -18,11 +18,19 @@ class TestDocumentor extends FunSpec with GivenWhenThen with Matchers
   describe("Documentor") {
     it("should create an empty table for an empty rule set") {
       val t = generateHtmlDocumentation( new SetEntry( "abc", Const(1) ) )
+      t should have size 1
       t.head should beXml (
         <tr>
         <td>abc</td>
         <td>1</td>
         </tr>)
+    }
+    it("should generate multiple rows when called with a chain") {
+      val t = generateHtmlDocumentation( SetEntry("k1",Const(1)) + SetEntry( "k2", Const(2)))
+      t should have size 2
+      t.head should beXml (<tr><td>k1</td><td>1</td></tr>)
+      t(1) should beXml (<tr><td>k2</td><td>2</td></tr>
+      )
     }
   }
 }
